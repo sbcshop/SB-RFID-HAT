7f3273529f573a0c489ba56d88a6cfa1bd90b2cc
satyam <satyam@thinkgenix.in>
2020-09-15 15:10:46 +0530

    fixed library issue

--- rfid_with_oled.py
+++ rfid_with_oled.py
@@ -1,71 +1,59 @@
-import serial 
-import time
+from oled_091 import SSD1306
+from subprocess import check_output
+from time import sleep
+from datetime import datetime
+from os import path
+import serial
+import RPi.GPIO as GPIO
 
-import Adafruit_SSD1306
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-import subprocess
-
-def read_rfid ():
-   ser = serial.Serial ("/dev/ttyS0")                           #Open named port 
-   ser.baudrate = 9600                                            #Set baud rate to 9600
-   data = ser.read(12)                                            #Read 12 characters from serial port to data
-   ser.close ()                                                   #Close port
-   data=data.decode("utf-8")
-   return data 
-
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-
-# 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+DIR_PATH = path.abspath(path.dirname(__file__))
+DefaultFont = path.join(DIR_PATH, "Fonts/GothamLight.ttf")
 
 
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
+class read_rfid:
+    def read_rfid (self):
+         
+        ser = serial.Serial ("/dev/ttyS0")                           #Open named port 
+        ser.baudrate = 9600                                            #Set baud rate to 9600
+        data = ser.read(12)                                            #Read 12 characters from serial port to data
+        if(data != " "):
+            GPIO.output(17,GPIO.HIGH)
+            sleep(.2)
+            GPIO.output(17,GPIO.LOW)
+        ser.close ()                                                   #Close port
+        data=data.decode("utf-8")
+        return data
 
 
-# Load default font.
-font = ImageFont.load_default()
+def info_print():
 
-while True:
-    id = read_rfid ()                                              #Function call
-    print (id) 
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # display.WhiteDisplay()
+    display.DirImage(path.join(DIR_PATH, "Images/SB.png"))
+    display.DrawRect()
+    display.ShowImage()
+    sleep(1)
+    display.PrintText(" ", FontSize=14)
+    display.ShowImage()
+    
+
+display = SSD1306()
+SB = read_rfid()
 
 
-    # Write two lines of text.
-    draw.text((x, top),       "ID: " + id,  font=font, fill=255)
 
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(.1)
+if __name__ == "__main__":
+    info_print()
+    while True:
+        id=SB.read_rfid()
+        print (id)
+        #CPU = info.CPU_Info()
+        # display.DirImage("Images/CPU.png", size=(24, 24), cords=(0, 0))
+        display.PrintText("ID : " +(id), cords=(4, 8), FontSize=11)
+        display.DrawRect()
+        display.ShowImage()
+        sleep(.1)
+        
